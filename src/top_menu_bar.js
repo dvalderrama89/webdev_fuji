b19dea23032882c5111768968f012ddcbcfc4d5d
David Valderrama <dvalderrama08@gmail.com>
2014-09-16 15:41:39 -0700

    Initial commit
    Implemented CSS spritesheets, GET reductions, juicebox gallery,
    javascript menu

---
+++ src/top_menu_bar.js
@@ -0,0 +1,121 @@
+$(document).ready(function(){
+	$("#navi1").mouseover(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_01")){
+				return "center_navi_01_roll";
+			}
+			else{
+				return "center_navi_01";
+			}	
+		});	
+	}).mouseleave(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_01")){
+				return "center_navi_01_roll";
+			}
+			else{
+				return "center_navi_01";
+			}	
+		});	
+	});
+
+	$("#navi2").mouseover(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_02")){
+				return "center_navi_02_roll";
+			}
+			else{
+				return "center_navi_02";
+			}	
+		});	
+	}).mouseleave(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_02")){
+				return "center_navi_02_roll";
+			}
+			else{
+				return "center_navi_02";
+			}	
+		});	
+	});
+
+	$("#navi3").mouseover(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_03")){
+				return "center_navi_03_roll";
+			}
+			else{
+				return "center_navi_03";
+			}	
+		});	
+	}).mouseleave(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_03")){
+				return "center_navi_03_roll";
+			}
+			else{
+				return "center_navi_03";
+			}	
+		});	
+	});
+
+	$("#navi4").mouseover(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_04")){
+				return "center_navi_04_roll";
+			}
+			else{
+				return "center_navi_04";
+			}	
+		});	
+	}).mouseleave(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_04")){
+				return "center_navi_04_roll";
+			}
+			else{
+				return "center_navi_04";
+			}	
+		});	
+	});
+
+	$("#navi5").mouseover(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_05")){
+				return "center_navi_05_roll";
+			}
+			else{
+				return "center_navi_05";
+			}	
+		});	
+	}).mouseleave(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_05")){
+				return "center_navi_05_roll";
+			}
+			else{
+				return "center_navi_05";
+			}	
+		});	
+	});
+
+	$("#navi6").mouseover(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_06")){
+				return "center_navi_06_roll";
+			}
+			else{
+				return "center_navi_06";
+			}	
+		});	
+	}).mouseleave(function(){
+		$(this).toggleClass(function(){
+			if($(this).is(".center_navi_06")){
+				return "center_navi_06_roll";
+			}
+			else{
+				return "center_navi_06";
+			}	
+		});	
+	});
+});
